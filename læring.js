22995de6e8fc12e914c5d23faa913831ee067383
EmilChristrupKjeldsen <70661473+EmilChristrupKjeldsen@users.noreply.github.com>
2020-10-29 08:58:59 +0100

    Add files via upload

---
+++ læring.js
@@ -0,0 +1,17 @@
+//I have used Alertify JS here
+/*Link to the framework's website:
+https://alertifyjs.com/
+*/
+
+function beskedDig() {
+    alertify.confirm('Er du tilfreds med denne side').set({title:"Emil spørger: "}).set({labels:{ok:'Ja', cancel: 'Nej'}}).set('closable', false);
+   
+
+}
+
+
+
+
+  
+
+
