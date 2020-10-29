3b9d7d70194073b562e6ef1ff705ecf1781863d6
EmilChristrupKjeldsen <emil679p@edu.aarhustech.dk>
2020-10-29 08:49:14 +0100

    First commit

---
+++ læring.js
@@ -0,0 +1,12 @@
+function beskedDig() {
+    alertify.confirm('Er du tilfreds med denne side').set({title:"Emil spørger: "}).set({labels:{ok:'Ja', cancel: 'Nej'}}).set('closable', false);
+   
+
+}
+
+
+
+
+  
+
+
